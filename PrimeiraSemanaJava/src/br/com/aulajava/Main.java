40445c40de90eec21d55eeebe1014d468f809475
Marcus Vinicius Bomfim Guimarães Barbalho <marcusbgb@gmail.com>
2020-11-09 21:40:58 -0300

    Adicionando Classe Main

---
+++ PrimeiraSemanaJava/src/br/com/aulajava/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.aulajava;
+
+/**
+ * @author marcu
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
